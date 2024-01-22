d9341fc3cf7ef4e3b9911dc409b35786801f94df
Viktor Palchynskyi <palchinskij19@gmail.com>
2024-01-22 10:34:56 +0300

    21.01.24 - skiing and new recipe

--- 07-authentication/01-local/libs/strategies/local.js
+++ 07-authentication/01-local/libs/strategies/local.js
@@ -1,8 +1,21 @@
 const LocalStrategy = require('passport-local').Strategy;
+const User = require('../../models/User');
 
 module.exports = new LocalStrategy(
     {usernameField: 'email', session: false},
-    function(email, password, done) {
-      done(null, false, 'Стратегия подключена, но еще не настроена');
+    async function(email, password, done) {
+      const user = User.findOne({email});
+
+      if (!user) {
+        done(null, false, 'Нет такого пользователя');
+      }
+      console.log(user);
+      const passwordHash = await user.schema.methods.checkPasswor(password);
+
+      if (!passwordHash) {
+        done(null, false, 'Нет такого пользователя');
+      }
+
+      done(user, false, 'User');
     },
 );
