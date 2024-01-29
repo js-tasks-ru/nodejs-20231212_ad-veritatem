a0dd913a151c42112e2bc29a24a1101008d8c0a0
Viktor Palchynskyi <palchinskij19@gmail.com>
2024-01-29 06:47:35 +0300

    29.01.24 - auth task 3

--- 07-authentication/03-sessions/app.js
+++ 07-authentication/03-sessions/app.js
@@ -33,6 +33,8 @@ app.use((ctx, next) => {
   ctx.login = async function(user) {
     const token = uuid();
 
+    await Session.create({token, lastVisit: Date.now(), user: user.id});
+
     return token;
   };
 
@@ -43,7 +45,21 @@ const router = new Router({prefix: '/api'});
 
 router.use(async (ctx, next) => {
   const header = ctx.request.get('Authorization');
+
   if (!header) return next();
+
+  const token = header.split(' ')[1];
+
+  if (!token) return next();
+
+  const session = await Session.findOne({token}).populate('user');
+
+  if (!session) {
+    ctx.throw(401, 'Неверный аутентификационный токен');
+  }
+
+  await session.update({lastVisit: Date.now()});
+  ctx.user = session.user;
 
   return next();
 });
@@ -53,7 +69,7 @@ router.post('/login', login);
 router.get('/oauth/:provider', oauth);
 router.post('/oauth_callback', handleMongooseValidationError, oauthCallback);
 
-router.get('/me', me);
+router.get('/me', mustBeAuthenticated, me);
 
 app.use(router.routes());
 
