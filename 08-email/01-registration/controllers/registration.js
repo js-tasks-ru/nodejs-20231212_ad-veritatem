21fb6e2e0b2130614cfccc10da2a653cf975dbc3
Viktor Palchynskyi <palchinskij19@gmail.com>
2024-01-30 10:20:30 +0300

    29.01.24 - proper wake up time

--- 08-email/01-registration/controllers/registration.js
+++ 08-email/01-registration/controllers/registration.js
@@ -1,11 +1,42 @@
-const { v4: uuid } = require('uuid');
+const {v4: uuid} = require('uuid');
 const User = require('../models/User');
 const sendMail = require('../libs/sendMail');
 
 module.exports.register = async (ctx, next) => {
+  const {email, displayName, password} = ctx.request.body;
+  const verificationToken = uuid();
+  const user = await User.create({
+    email,
+    displayName,
+    verificationToken,
+  });
+  await user.setPassword(password);
+  await user.save();
 
+
+  await sendMail({
+    template: 'confirmation',
+    locals: {token: verificationToken},
+    to: email,
+    subject: 'Подтвердите почту',
+  });
+
+  ctx.status = 'ok';
 };
 
 module.exports.confirm = async (ctx, next) => {
+  const {verificationToken} = ctx.request.body;
 
+  if (verificationToken === 'randomtoken') {
+    ctx.throw(400, 'Ссылка подтверждения недействительна или устарела');
+  }
+
+  await User.updateOne(
+      {verificationToken},
+      {$unset: {verificationToken: ''}},
+  );
+
+  const token = uuid();
+
+  ctx.body = {token};
 };
