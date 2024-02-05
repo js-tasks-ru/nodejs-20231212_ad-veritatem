61df170d4773ac9db45a4b12c2298ece23969b36
Viktor Palchynskyi <palchinskij19@gmail.com>
2024-02-05 11:31:31 +0300

    05.02.24 - websocket task 1

--- 09-websockets/01-chat/controllers/messages.js
+++ 09-websockets/01-chat/controllers/messages.js
@@ -1,5 +1,10 @@
 const Message = require('../models/Message');
+const mapMessage = require('../mappers/message');
 
 module.exports.messageList = async function messages(ctx, next) {
-  ctx.body = {};
+  const user = ctx.user;
+  const messagesRes = await Message.find({chat: user.id}).limit(20);
+  const messages = messagesRes.map(mapMessage);
+
+  ctx.body = {messages};
 };
